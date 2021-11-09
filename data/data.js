5320a67ff017b31d989c49c8cf30cceec7055684
jaimeviloriogreen <jaimeviloriogreen@hotmail.com>
2021-11-08 21:33:29 -0400

    five questions added

--- data/data.js
+++ data/data.js
@@ -10,10 +10,26 @@ export const data = [
         answer:1
     },
     {
+        question:"Estudia descriptivamente las razas y los pueblos", 
+        choices:["La Antropología", "La Psicología", "La Sociología", "La Etnografía"],
+        answer:3
+
+    },
+    {
         question:"Es considerada la cuna de las civilizaciones", 
         choices:["Roma de Occidente", "Bizancio", "Constantinopla", "Mesopotamia"],
         answer:3
     }, 
+    {
+        question:"Estudia al ser humano y su conformación física y cultural", 
+        choices:["La Antropología", "La Geografía", "La Arqueología", "La Economía"], 
+        answer:0
+    },
+    {
+        question:"Período de la humanidad anterior a todo documento escrito",
+        choices:["El Neolítico", "La Prehistoria", "La Edad Media", "El Paleolítico"], 
+        answer:1
+    },
     {
         question:"¿Quiénes inventaron la democracia?", 
         choices:["Los Griegos","Los Romanos", "Los Turcos", "Los Egipcios"],
@@ -23,5 +39,14 @@ export const data = [
         question:"¿Quién es considerado el padre de la historia?", 
         choices:["Cicéron","Homero", "Socrátes", "Heródoto"],
         answer:3
+    }, 
+    {
+        question:"Período en el que el hombre pasa del nomadismo al sedentarismo", 
+        choices:["La Edad Moderna", "El Paleolítico", "El Neolítico", "El medioevo"]
+    },
+    {
+        question:"Estudia el pasado relevante de la humanidad", 
+        choices:["La Economía", "La Etnografía", "La Historia", "La Paleontología"],
+        answer:2
     }
 ];
